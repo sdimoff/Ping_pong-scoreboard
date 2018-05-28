056f5e5df174d88d16c0398633b3426707d0625a
Tsvetan Dimoff <tsdimo@gmail.com>
2018-05-28 17:22:17 +0300

    Acceptance test added

--- tests/acceptance/add-game-test.js
+++ tests/acceptance/add-game-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit } from '@ember/test-helpers';
+import { visit, andThen } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | add game', function(hooks) {
@@ -8,6 +8,11 @@ module('Acceptance | add game', function(hooks) {
   test('New match is added in the scoreboard when the user clicks the button', function(assert) {
     visit('/');
 
+    andThen(function() {
+      const matchNumber = $('.match').length;
 
+      $('.add-game__button').click();
+      assert.equal($('.match').length, matchNumber + 1, 'New match has been added');
+    })
   });
 });
